b4d609c4289f474363cb4e0aa37b7755c26231e4
Fraxinas <photobooth@schaffenburg.org>
2017-04-22 15:04:12 +0200

    PhotoBoothWindow: start all countdown tick callbacks

--- photoboothwin.c
+++ photoboothwin.c
@@ -164,9 +164,8 @@ gboolean _pbw_tick_countdown (PhotoBoothWindow *win)
 	else if (priv->countdown == -1)
 	{
 		gtk_widget_hide (GTK_WIDGET (priv->countdown_label));
-		return FALSE;
 	}
-	return TRUE;
+	return FALSE;
 }
 
 void photo_booth_window_hide_cursor (PhotoBoothWindow *win)
@@ -191,7 +190,11 @@ void photo_booth_window_start_countdown (PhotoBoothWindow *win, gint count)
 	priv->countdown = count+1;
 	_pbw_tick_countdown(win);
 	gtk_widget_show (GTK_WIDGET (priv->countdown_label));
-	g_timeout_add (1000, (GSourceFunc) _pbw_tick_countdown, win);
+  for (int i = 1; i <= count+2; i++)
+  {
+    g_timeout_add (1000*i, (GSourceFunc) _pbw_tick_countdown, win);
+    GST_INFO ("added timeout callback at %i", 1000*i);
+  }
 }
 
 void photo_booth_window_set_copies_show (PhotoBoothWindow *win, gint min, gint max, gint def)
